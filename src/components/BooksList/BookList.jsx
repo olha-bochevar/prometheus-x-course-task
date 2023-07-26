d55dcf990c478cefd3636e8454a8b9113f97e772
olha-bochevar <olya.bochevarland@gmail.com>
2023-07-26 16:27:08 +0300

    Add styles to BooksList section

--- src/components/BooksList/BookList.jsx
+++ src/components/BooksList/BookList.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useBooks } from '../../hooks/BooksContext';
 import imageNotFound from './../../assets/images/imageNotFound.png';
-import './book-list.css';
+import './BookList.css';
+
 export function BookList() {
 	const { books } = useBooks();
 	const [search, setSearch] = useState('');
@@ -33,45 +34,79 @@ export function BookList() {
 		book.title.toLowerCase().includes(search.toLowerCase())
 	);
 
+	// для max-width : 420px будемо показувати повну назву, для більших екранів - скорочуємо назву до 24 символів
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+	});
+
+	const handleResize = () => {
+		setWindowSize({
+			width: window.innerWidth,
+		});
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	const sliceNameOfBook = (title) => {
+		return title.length > 24 ? title.substring(0, 24).concat('...') : title;
+	};
+
 	return (
-		<section className="bookList">
-			<div>
-				<form>
+		<section className="booklist">
+			<h1>Вітаємо! Обирайте книжечки!</h1>
+			<div className="booklist__container">
+				<div className="booklist__filters">
 					<input
 						type="search"
 						value={search}
 						onChange={handleSearchInputValue}
+						className="booklist__filter-by-name"
+						placeholder="Введіть назву книги"
 					/>
-					<select value={filterPrice} onChange={handleChangeFilterPrice}>
+					<select
+						value={filterPrice}
+						onChange={handleChangeFilterPrice}
+						className="booklist__filter-by-price"
+					>
 						<option value="all" key="">
 							Всі
 						</option>
-						<option value="<15" key="15">{`0 < ціна < 15`}</option>
-						<option value="<30" key="30">{`15 < ціна < 30`}</option>
-						<option value="30+" key="30+">{`ціна > 30`}</option>
+						<option value="<15" key="15">{`0$ < Ціна < 15$`}</option>
+						<option value="<30" key="30">{`15$ < Ціна < 30$`}</option>
+						<option value="30+" key="30+">{`Ціна > 30$`}</option>
 					</select>
-				</form>
-			</div>
-			<div className="books-container">
-				<div className="book-container">
-					{filteredBooks.map((book) => (
-						<li key={book?.id}>
-							<div>
+				</div>
+				<div className="booklist__catalog">
+					<ul className="booklist__list">
+						{filteredBooks.map((book) => (
+							<li key={book?.id} className="booklist__book book">
 								<img
 									src={book?.image ? book.image : imageNotFound}
 									alt="Book's cover"
+									className="book__image"
 								/>
-								<h5>
-									{book?.title.length > 24
-										? book.title.substring(0, 24).concat('...')
-										: book.title}
-								</h5>
-								<p>{book?.author}</p>
-								<span>{book?.price}</span>
-								<Link to={`/book/${book?.id}`}>View</Link>
-							</div>
-						</li>
-					))}
+
+								<h3 className="book__title">
+									{windowSize.width <= 420
+										? book.title
+										: sliceNameOfBook(book.title)}
+								</h3>
+								<p className="book__author">{book?.author}</p>
+								<div className="book__info">
+									<span className="book__price">{`${book?.price}$`}</span>
+									<Link to={`/book/${book?.id}`} className="book__link">
+										View
+									</Link>
+								</div>
+							</li>
+						))}
+					</ul>
 				</div>
 			</div>
 		</section>
--- src/routes/ProductsPage.jsx
+++ src/routes/ProductsPage.jsx
@@ -2,7 +2,6 @@ import { BookList } from '../components/BooksList/BookList';
 export function ProductsPage() {
 	return (
 		<>
-			<h1>Book list</h1>
 			<BookList />
 		</>
 	);
